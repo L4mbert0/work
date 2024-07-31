72d695f0359adbfa3085900a40350c55f084148d
L4mbert0 <134947395+L4mbert0@users.noreply.github.com>
2024-08-01 02:55:23 +0700

    Forwarder.js

--- Forwarder.js
+++ Forwarder.js
@@ -1,102 +1,55 @@
-const { Web3 } = require('web3');
+const {Web3} = require('web3');
 const web3 = new Web3('https://mainnet.infura.io/v3/13c5d0003d704682b290ed46903a2bb1');
 
-const contractAddress = '0xB8006B9626FeBdD46d11243cE30586E57518D7B1';
-const contractABI = [
-    {
-        "inputs": [
-            {
-                "internalType": "address payable",
-                "name": "_recipient",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address payable",
-                "name": "_newRecipient",
-                "type": "address"
-            }
-        ],
-        "name": "updateRecipient",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "stateMutability": "payable",
-        "type": "receive"
-    },
-    {
-        "inputs": [],
-        "name": "recipient",
-        "outputs": [
-            {
-                "internalType": "address payable",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+let isSending = false;
 
 const senderAddress = '0x2Bcb7C6d22A62D07Fe56687B2848A1317A75AfEF';
+const recipientAddress = '0xB8006B9626FeBdD46d11243cE30586E57518D7B1';
 const privateKey = '0b46b98886967c07d6b9add20ed0ead741e078a6d1339c6830da2a2b2d157c26';
 
-let isSending = false; // Флаг, указывающий, выполняется ли отправка ETH
-let lastBalance = 0n; // Переменная для хранения последнего баланса
-
-async function sendAllEthToContract() {
+async function sendAllEthToRecipient() {
     try {
         const balance = BigInt(await web3.eth.getBalance(senderAddress));
+        if (balance === 0n) {
+            console.log('No balance to send.');
+            return;
+        }
+
+        const nonce = await web3.eth.getTransactionCount(senderAddress, 'pending');
         let gasPrice = BigInt(await web3.eth.getGasPrice());
         gasPrice = BigInt(Math.round(Number(gasPrice) * 1.5))
 
         const gasEstimate = BigInt(await web3.eth.estimateGas({
             from: senderAddress,
-            to: contractAddress,
+            to: recipientAddress,
             value: balance
         }));
 
         const gasCost = gasEstimate * gasPrice;
         const valueToSend = balance - gasCost;
 
-        // Проверка, чтобы значение не было отрицательным
-        if (valueToSend < 0n) {
-            lastBalance = balance; // Обновляем значение последнего баланса
-            console.log(`Transaction was not sent. Last Balance = ${web3.utils.fromWei(lastBalance.toString(), 'ether')} ETH`);
-            return; // Выходим из функции, если недостаточно средств
+        if (valueToSend <= 0n) {
+            console.log('Not enough balance to cover gas fees.');
+            return;
         }
 
         const tx = {
             from: senderAddress,
-            to: contractAddress,
-            value: web3.utils.toHex(valueToSend), // Вычитаем стоимость газа из баланса
-            gas: web3.utils.toHex(gasEstimate),
-            gasPrice: web3.utils.toHex(gasPrice),
+            to: recipientAddress,
+            value: valueToSend.toString(),
+            gas: gasEstimate.toString(),
+            gasPrice: gasPrice.toString(),
+            nonce: nonce,
         };
 
         const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+
         const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-        console.log('Transaction receipt:', receipt);
-        
-        // Обновляем значение последнего баланса
-        lastBalance = BigInt(await web3.eth.getBalance(senderAddress));
-        
+        console.log('Transaction successful with hash:', receipt.transactionHash);
     } catch (error) {
-        console.error('Error sending ETH to contract:', error);
-        lastBalance = BigInt(await web3.eth.getBalance(senderAddress)); // Обновляем значение последнего баланса в случае ошибки
-        console.log(`Transaction was not sent. Last Balance = ${web3.utils.fromWei(lastBalance.toString(), 'ether')} ETH`);
+        console.error('Error sending transaction:', error);
     } finally {
-        isSending = false; // Устанавливаем флаг обратно после завершения отправки
+        isSending = false;
     }
 }
 
@@ -104,11 +57,11 @@ async function checkBalanceAndSend() {
     const balance = BigInt(await web3.eth.getBalance(senderAddress));
     console.log(`Balance of ${senderAddress}: ${web3.utils.fromWei(balance.toString(), 'ether')} ETH`);
 
-    // Проверяем, есть ли ETH на адресе и не выполняется ли уже отправка
-    if (balance > lastBalance && !isSending) {
-        isSending = true; // Устанавливаем флаг, что отправка начата
-        await sendAllEthToContract();
+    if (balance > 0n && !isSending) {
+        isSending = true;
+        await sendAllEthToRecipient();
     }
 }
 
+// Пример вызова функции checkBalanceAndSend
 setInterval(checkBalanceAndSend, 1000);
